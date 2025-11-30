09b6bb6c4eb45cc61afa18421a3e40084c4240d5
werlleyBarros <werlleysousa2019@gmail.com>
2025-11-30 14:21:11 +0000

    atualização script login/cadastro

--- client/script.js
+++ client/script.js
@@ -6,11 +6,11 @@ async function Cadastro() {
     const senha = document.getElementById("senhaImput").value;
 
     const payLoad = {
-        nome: nome,
+        name: nome,
         email: email,
-        senha: senha,
+        password: senha,
     };
-
+    console.log(payLoad)
 
     try {
         const resp = await fetch(url, {
@@ -30,6 +30,38 @@ async function Cadastro() {
 
     } catch(erro) {
         console.error(erro);
+        alert("Deu ruim ao enviar formulario")
+    }
+};
+
+async function login(){
+    const url = "https://verbose-fishstick-v6jjqpv7ppxgcpjx9-8000.app.github.dev/auth/login"
+    const email = document.getElementById("emailImput").value;
+    const senha = document.getElementById("senhaImput").value;
+
+    const payLoad = {
+        email: email,
+        password: senha,
+    }
+    console.log(payLoad);
+    try{
+        const resp = await fetch(url,{
+            method: 'POST',
+            body: JSON.stringify(payLoad),
+            headers: {'Content-Type': 'application/json',},
+        });
+
+        if (!resp.ok){
+            throw new Error("Erro ao enviar formulario");
+        }
+
+        const respJson = await resp.json();
+        
+        console.log(respJson);
+        alert("Sucesso ao enviar")
+
+    } catch(erro){
+        console.error(erro)
         alert("Deu ruim ao enviar formulario")
     }
 }
