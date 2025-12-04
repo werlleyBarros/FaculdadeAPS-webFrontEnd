5cf1081bb716096531955ad880838c1e5ed4016b
werlleyBarros <werlleysousa2019@gmail.com>
2025-12-04 03:29:33 +0000

    atualização script

--- client/script.js
+++ client/script.js
@@ -72,7 +72,6 @@ async function login(){
     }
 };
 
-
 function criarTaskCard(task){
 
     const card = document.createElement("div");
@@ -94,26 +93,120 @@ function criarTaskCard(task){
     const botaoEditar = document.createElement("button");
     botaoEditar.textContent = "Editar";
     botaoEditar.classList.add("botaoEditar");
-    botaoEditar.setAttribute('onclick', "editarTask");
+    botaoEditar.setAttribute('onclick', 'editarTask('+ task.id +')');
     card.appendChild(botaoEditar);
 
     const botaoDeletar = document.createElement("button");
     botaoDeletar.textContent = "Deletar";
     botaoDeletar.classList.add("botaoDeletar");
-    botaoDeletar.setAttribute('onclick', "deleteTask");
+    botaoDeletar.setAttribute('onclick', 'deleteTask('+ task.id +')');
     card.appendChild(botaoDeletar);
 
-    const container = document.getElementById("tarefasContainer()");
+    const container = document.getElementById("tarefasContainer");
     container.appendChild(card);
 
 };
 
-function editarTask(){
-    console.log("editou")
+function editarTask(taskId){
+    const formEditarTask = document.getElementById("tarefa-" + taskId)
+
+    if (formEditarTask.querySelector("form")) return;
+
+    const tituloAtual = formEditarTask.querySelector("h3").innerText;
+    const descricaoAtual = formEditarTask.querySelector("p").innerText;
+
+    const form = document.createElement("form");
+
+    const inputTitulo = document.createElement("input");
+    inputTitulo.type = "text";
+    inputTitulo.name = "novoTitulo";
+    inputTitulo.id = "novoTitulo";
+    inputTitulo.value = null;
+    inputTitulo.placeholder = "Novo Titulo"
+    form.appendChild(inputTitulo);
+
+    const inputDescricao = document.createElement("input");
+    inputDescricao.type = "text";
+    inputDescricao.name = "novaDescricao";
+    inputDescricao.id = "novaDescricao";
+    inputDescricao.value = null;
+    inputDescricao.placeholder = "Nova descrição"
+    form.appendChild(inputDescricao);
+
+    const inputData = document.createElement("input");
+    inputData.type = "date";
+    inputData.name = "novaData";
+    inputData.id = "novaData";
+    inputData.value = null;
+    form.appendChild(inputData);
+
+    const botaoSalvar = document.createElement("button");
+    botaoSalvar.type = "submit";
+    botaoSalvar.textContent = "Salvar";
+    botaoSalvar.setAttribute('onclick', "EnviarTaskEdicao(" + taskId + ")")
+    form.appendChild(botaoSalvar);
+
+    formEditarTask.querySelector("h3").style.display = "none";
+    formEditarTask.querySelector("p").style.display = "none";
+    formEditarTask.appendChild(form);
+
 };
 
-function deleteTask(){
-    console.log("deletou")
+async function EnviarTaskEdicao(taskId){
+    event.preventDefault();
+    
+    const url = baseUrl + "/tasks/" + taskId
+    console.log(url)
+    const titulo = document.getElementById("novoTitulo").value;
+    const descrição = document.getElementById("novaDescricao").value;
+    const prazoEntrega = document.getElementById("novaData").value;
+
+    const payload = {
+        title: titulo,
+        description: descrição,
+        deadline: prazoEntrega,
+    };
+
+    try {
+        const resp = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(payload),
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": "Bearer " + localStorage.getItem("Token")
+            },
+        });
+        console.log(resp);
+
+        if (!resp.ok) {
+            throw new Error("Erro ao enviar formulario");
+            
+        };
+        alert("Deu certo");
+
+    } catch (erro) {
+        console.error(erro);
+        alert("Erro ao enviar sua formulario");
+    };
+};
+
+async function deleteTask(taskId){
+    const url = baseUrl + "/tasks/" + taskId
+    try{
+        const resp = await fetch(url, {
+           method: "DELETE",
+           headers: {
+            'Content-Type': 'application/json',
+            "authorization": "Bearer " + localStorage.getItem("Token"),
+        },
+           
+        });
+
+    }catch(erro){
+        console.error(erro)
+        alert("Deu ruim ao enviar formulario")
+    };
+
 };
 
 async function carregarTasks() {
@@ -154,6 +247,8 @@ function mostrarFormulario(){
 };
 
 async function criarTask(){
+    event.preventDefault();
+    
     const url = baseUrl + "/tasks"
 
     const titulo = document.getElementById("tituloTarefa").value;
@@ -183,6 +278,6 @@ async function criarTask(){
 
     } catch (erro) {
         console.error(erro);
-        alert("Erro ao enviar formulario");
+        alert("Erro ao enviar sua formulario");
     };
 };
