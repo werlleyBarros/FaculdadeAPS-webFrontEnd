650eb5c5fddc35fafdb7ef55b97da66261e22a3f
Lucas Ferreira Dias <lucas.f.d2001@gmail.com>
2025-12-07 03:13:52 +0000

    pequena mudança no css

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://expert-yodel-pjpwgqq9jq7wh6rj9-8000.app.github.dev";
+const baseUrl = "https://glowing-adventure-v6qrg77w64wr3p595-8000.app.github.dev";
 
 async function apiRequest(endpoint, method = "GET", body = null, usaToken = true){
     const url = baseUrl + endpoint
